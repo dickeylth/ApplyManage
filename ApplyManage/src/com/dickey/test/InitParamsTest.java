57c0a1ce3b5381f8e0f5bb3c601b99f865afd2c7
Dickey <dickeylth@gmail.com>
2013-01-18 14:17:09 +0800

    初始化的测试代码
    Change-Id: Ie712a5a8a46d5b03b5e1416e3fdd2a83f1bff898

--- ApplyManage/src/com/dickey/test/InitParamsTest.java
+++ ApplyManage/src/com/dickey/test/InitParamsTest.java
@@ -1,14 +1,9 @@
 package com.dickey.test;
 
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 
-import com.dickey.dao.PermissionDao;
-import com.dickey.dao.RoleDao;
-import com.dickey.dao.UserDao;
 import com.dickey.domain.Permission;
 import com.dickey.domain.Role;
 import com.dickey.service.UserService;
@@ -21,32 +16,22 @@ public class InitParamsTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		ApplicationContext act = new FileSystemXmlApplicationContext("WebContent/WEB-INF/applicationContext.xml");
-		/*UserService userService = act.getBean("userService", UserService.class);
+		UserService userService = act.getBean("userService", UserService.class);
 		
+		//先处理permission
 		Permission permission = new Permission();
 		permission.setPermission("TEST_PERMISSION");
 		
 		userService.addPermission(permission);
 		
+		//再处理role
 		Role role = new Role();
 		role.setRolename("ROLE_TEST");
 		role.getPermissions().add(permission);
 		
-		userService.addRole(role);*/
-		UserDao userDao = act.getBean("userDao", UserDao.class);
-		RoleDao roleDao = act.getBean("roleDao", RoleDao.class);
-		PermissionDao permissionDao = act.getBean("permissionDao", PermissionDao.class);
+		userService.addRole(role);
 		
-		Permission permission = new Permission();
-		permission.setPermission("TEST_PERMISSION");
-		permissionDao.save(permission);
-		
-		Role role = new Role();
-		role.setRolename("ROLE_TEST");
-		role.getPermissions().add(permission);
-		
-		roleDao.save(role);
-		
+		//最后处理user
 		
 
 	}
